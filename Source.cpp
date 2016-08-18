84263063391f014a2bb2c9979ded30722134e37a
matthewcicero1139 <matthewcicero33@gmail.com>
2016-08-18 16:40:05 -0500

    Excercise Variables
    Swap numbers

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+int main()
+{
+
+		int a = 3;
+		int b = 5;
+		int temp = 0;
+		 temp = a;
+		 a = b;
+		 b = temp;
+		 
+		//int a = 3;
+		//int b = temp;
+		//int temp = a;
+
+
+		 printf("%i", a);
+		 printf("%i", b);
+	
+	system("pause");
+	return 0;
+}
