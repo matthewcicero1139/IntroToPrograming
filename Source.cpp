3f3d0ec5a1a0d781f04c2a856916352baa637c59
matthewcicero1139 <matthewcicero33@gmail.com>
2016-08-19 13:35:30 -0500

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,22 +1,9 @@
 #include <iostream>
 int main()
 {
-
-		int a = 3;
-		int b = 5;
-		int temp = 0;
-		 temp = a;
-		 a = b;
-		 b = temp;
-		 
-		//int a = 3;
-		//int b = temp;
-		//int temp = a;
-
-
-		 printf("%i", a);
-		 printf("%i", b);
-	
+	int x = 0;
+	int y = 10;
+	int k = (x > y) ? x : y;
+	printf("%i", k);
 	system("pause");
-	return 0;
 }
