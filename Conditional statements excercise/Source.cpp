b7e5a1e2a4bd989913267e2dfb4ae8d383420868
1139 <matthewcicero33@gmail.com>
2016-08-19 17:01:30 -0500

    Conditionals Excersize

---
+++ Conditional statements excercise/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+int main()
+{
+	int m = 3;
+	int k = 5;
+	//std::cin >> m;
+	//std::cin >> k;
+	if (m >= 8)
+	{
+		printf("%i", m);
+		system("pause");
+	}
+	else
+	{
+		printf("%i", k);
+		system("pause");
+		return 0;
+	}
+}
