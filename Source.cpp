75c8d9671691ba8f04495ba738f2b03e1d0556a0
matthewcicero1139 <matthewcicero33@gmail.com>
2016-08-23 14:50:46 -0500

    Add files via upload
    Conditionals Exercise 2

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 int main()
 {
-	int x = 0;
-	int y = 10;
-	int k = (x > y) ? x : y;
-	printf("%i", k);
-	system("pause");
+	int m = 3;
+	int k = 5;
+	//std::cin >> m;
+	//std::cin >> k;
+	if (m >= 8)
+	{
+		printf("%i", m);
+		system("pause");
+	}
+	else
+	{
+		printf("%i", k);
+		system("pause");
+		return 0;
+	}
 }
