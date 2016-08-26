dd8fdeeee9223a248881bb8e3818ecf12cdeac58
matthewcicero1139 <matthewcicero33@gmail.com>
2016-08-26 11:03:53 -0500

    Update Variable Exercise 7.cpp

--- Variables/Variable Exercise 7.cpp
+++ Variables/Variable Exercise 7.cpp
@@ -1,10 +1,3 @@
-//	Write a program that determines the letter that lies halfway between two letters of the
-//	alphabet as input by the user.For example, if the user inputs ‘A’ and ‘Z’, the output should
-//	be ‘M’.Do you know how to deal with upper vs.lower case letters ? Does it matter if the
-//	letters are input in reverse order ?
-//	Note : Each character is assigned a value defined by the ASCII(American Standard Code for
-//	Information Interchange) character table.
-
 #include <iostream>
 int main()
 {
